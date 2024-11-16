a8c701b2f70b99855661e52de40e5f55c5d2a0b1
Dhanuka Rathnayaka <137430860+Dhanuka001@users.noreply.github.com>
2024-11-16 13:10:18 +0530

    favorite feature added

--- client/src/NoteCard.jsx
+++ client/src/NoteCard.jsx
@@ -2,19 +2,36 @@ import React, { useState } from 'react';
 import { IoTrash } from 'react-icons/io5';
 import { MdOutlineDone } from "react-icons/md";
 import { MdModeEdit } from "react-icons/md";
+import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 
-function NoteCard({ color, onDelete }) {
-  const [content, setContent] = useState('');
+function NoteCard({color, onDelete,content: initialContent }) {
+  
+  const [content, setContent] = useState(initialContent || '');
   const [isEditing, setIsEditing] = useState(true);
+  const [favorite, setFavorite] = useState(false);
 
   const handleSave = () => setIsEditing(false);
   const handleEdit = () => setIsEditing(true);
 
+  const toggleFavorite = () => setFavorite(!favorite);
+
   return (
     <div
-      className="w-52 h-52 p-4 rounded-xl shadow-md flex flex-col justify-between"
+      className="relative w-52 h-52 p-4 rounded-xl shadow-md flex flex-col justify-between"
       style={{ backgroundColor: color }}
     >
+       {/* Favorite Button */}
+       <button
+        onClick={toggleFavorite}
+        className="absolute top-2 right-2 text-xl"
+      >
+        {favorite ? (
+          <AiFillStar className="text-yellow-500" />
+        ) : (
+          <AiOutlineStar className="text-gray-400" />
+        )}
+      </button>
+
       {isEditing ? (
         <textarea
           value={content}
@@ -29,21 +46,21 @@ function NoteCard({ color, onDelete }) {
         {isEditing ? (
           <button
             onClick={handleSave}
-            className="px-2 py-2 bg-black text-white rounded-full "
+            className="px-1 py-1 bg-black text-white rounded-full "
           >
             <MdOutlineDone />
           </button>
         ) : (
           <button
             onClick={handleEdit}
-            className="px-2 py-2 bg-black text-white rounded-full"
+            className="px-1 py-1 bg-black text-white rounded-full"
           >
             <MdModeEdit/>
           </button>
         )}
         <button
           onClick={onDelete}
-          className="px-2 py-2 bg-black text-white rounded-full "
+          className="px-1 py-1 bg-black text-white rounded-full "
         >
           <IoTrash/>
         </button>
